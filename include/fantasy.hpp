52ce085b734a516762a0d3a7f55d0f1c8393f43f
Sachit Dabas <sachitdabas@Sachits-MacBook-Pro.local>
2020-04-05 20:32:59 -0700

    wip actions code

--- include/fantasy.hpp
+++ include/fantasy.hpp
@@ -7,14 +7,11 @@ CONTRACT fantasy : public contract {
   public:
     using contract::contract;
 
-    // action to add new user
-    ACTION adduser(name user);
-
-    // action to update verification status user
-    ACTION verifyuser(name user);
+    // action to add new user and modify user status.
+    ACTION addmoduser(name user, uint8_t status);
 
     // action to add new distrbution event and associated options for token distribution
-    ACTION regdistevent(event_id, vector<uint8_t> option_ids, date event_close_time);
+    ACTION regdistevent(uint32_t event_id, vector<uint8_t> option_ids, uint64_t event_close_time);
 
     // action to add winning option for a distribution event
     ACTION addoutcome(event_id, option_id);
@@ -38,6 +35,54 @@ CONTRACT fantasy : public contract {
   
 
   private:
+
+    enum user_status: uint8_t {
+      NOT_VERIFIED= 0,
+      VERIFIED=1,
+      BLACK_LIST=2 
+    };
+
+    TABLE registered_users {
+      name user;
+      uint8_t user_status;
+      auto primary_key() const { return user.value; }
+    };
+    typedef multi_index<name("users"), registered_users> users_table;
+
+   enum event_status: uint8_t {
+      INITIATING= 0,
+      VOTE_STARTING= 1,
+      VOTE_ENDED= 2,
+      CLOSED= 3,
+      DISTRIBUTION_STARTED= 4,
+      DISTRIBUTION_ENDED= 5 
+    };
+
+    TABLE distribution_event_registration {
+      uint32_t event_id;
+      vector<uint32_t> option_ids;
+      uint32_t event_close_time;
+      uint8_t event_status = INITIATING;
+      uint32_t outcome_option_id;
+      uint32_t distribution_end_time;
+      auto primary_key() const { return event_id; }
+    };
+    typedef multi_index<name("disteventreg"), distribution_event_registration> distribution_event_registration_table;
+
+    TABLE distribution_user_selection {
+      uint128_t selection_id;
+      name user;
+      uint32_t  event_id;
+      uint32_t option_id;
+      auto primary_key() const { return selection_id; }
+      uint128_t user_key() const {
+        return user.value;
+      }
+    };
+    typedef multi_index<name("distusersel"), distribution_user_selection,
+    indexed_by<name("userkey"), 
+    const_mem_fun<distribution_user_selection, uint128_t, &distribution_user_selection::user_key>>
+    > distribution_user_selection_table;
 
     struct player {
       uint32_t player_id;
@@ -72,51 +117,4 @@ CONTRACT fantasy : public contract {
       auto primary_key() const { return user_selection_id; }
     };
     typedef multi_index<name("fantasyus"), fantasy_user_selection> fantasy_user_selection_table;
-
-    enum user_status: uint8_t {
-      NOT_VERIFIED= 0,
-      VERIFIED=1 
-    };
-
-    TABLE registered_users {
-      name user;
-      uint8_t user_status;
-      auto primary_key() const { return user.value; }
-    };
-    typedef multi_index<name("users"), registered_users> users_table;
-
-   enum event_status: uint8_t {
-      INITIATING= 0,
-      VOTE_STARTING= 1,
-      VOTE_ENDED= 2,
-      CLOSED= 3,
-      DISTRIBUTION_STARTED= 4,
-      DISTRIBUTION_ENDED= 5 
-    };
-
-    TABLE distribution_event_registration {
-      uint32_t event_id;
-      vector<uint32_t> option_id;
-      date event_close_time;
-      uint8_t event_status = INITIATING;
-      uint32_t outcome_option_id;
-      date distribution_end_time;
-      auto primary_key() const { return event_id; }
-    };
-    typedef multi_index<name("disteventreg"), distribution_event_registration> distribution_event_registration_table;
-
-    TABLE distribution_user_selection {
-      uint128_t selection_id;
-      name user;
-      uint32_t  event_id;
-      uint32_t option_id;
-      auto primary_key() const { return selection_id; }
-      uint128_t user_key() const {
-        return user.value;
-      }
-    };
-    typedef multi_index<name("distusersel"), distribution_user_selection,
-    indexed_by<name("userkey"), 
-    const_mem_fun<distribution_user_selection, uint128_t, &distribution_user_selection::user_key>>
-    > distribution_user_selection_table;
 };
--- src/fantasy.cpp
+++ src/fantasy.cpp
@@ -1,17 +1,102 @@
 #include <fantasy.hpp>
+#include <eosio/system.hpp>
 
-ACTION fantasy::adduser(name user) {
+
+ACTION fantasy::addmoduser(name user, uint8_t status) {
   require_auth(get_self());
 
   users_table _users_table(get_self(), get_self().value);
 
-  auto users_itr = _users_table.find(user.value);
-  if (users_itr == _users_table.end()) {
-    // Create a message record if it does not exist
-    _users_table.emplace(get_self(), [&](auto& row) {
+  auto user_itr = _users_table.find(user.value);
+
+  if (user_itr == _users_table.end()) {
+    _user_table.emplace(get_self(), [&](auto& row) {
       row.user = user;
+      row.user_status = status;
+    });
+  } else {
+    _users_table.modify(user_itr, get_self(), [&](auto& row){
+          row.user_status = status;
     });
   }
+}
+
+ACTION fantasy::regdistevent(uint32_t event_id, vector<uint8_t> option_ids, uint64_t event_close_time){
+  require_auth(get_self());
+
+  distribution_event_registration_table _distribution_event_registration_table(get_self(), get_self().value);
+
+  auto event_itr = _distribution_event_registration_table.find(event_id);
+
+  if(event_itr == _distribution_event_registration_table.end()) {
+    _distribution_event_registration_table.emplace(get_self(), [&](auto& row){
+      row.event_id = event_id;
+      row.option_ids = option_id;
+      row.event_close_time = event_close_time;
+      row.event_status = INITIATING;
+    });
+  } else {
+      eosio::check(false, "Event already exists. Update event using other actions");
+  }
+}
+
+ACTION fantasy::upddisoption(uint32_t event_id, vector<uint8_t> option_ids) {
+  require_auth(get_self());
+
+  distribution_event_registration_table _distribution_event_registration_table(get_self(), get_self().value);
+
+  auto& event_itr = _distribution_event_registration_table.get(event_id, "Invalid eventId");
+
+  _distribution_event_registration_table.modify(event_itr, get_self(), [&](auto& row){row.option_ids = option_ids;});
+
+}
+
+
+ACTION fantasy::upddisclotim(uint32_t event_id, uint64_t event_close_time) {
+  require_auth(get_self());
+
+  distribution_event_registration_table _distribution_event_registration_table(get_self(), get_self().value);
+
+  auto& event_itr = _distribution_event_registration_table.get(event_id, "Invalid eventId");
+
+  _distribution_event_registration_table.modify(event_itr, get_self(), [&](auto& row){row.event_close_time = event_close_time;});
+
+}
+
+
+ACTION fantasy::addoutcome(uint32_t event_id, uint32_t winning_option_id) {
+  require_auth(get_self());
+
+  distribution_event_registration_table _distribution_event_registration_table(get_self(), get_self().value);
+
+  auto& event_itr = _distribution_event_registration_table.get(event_id, "Invalid eventId");
+
+  _distribution_event_registration_table.modify(event_itr, get_self(), [&](auto& row)
+    {row.outcome_option_id = winning_option_id;
+    row.event_status = CLOSED;});
+}
+
+ACTION fantasy::adddisusrsel(name user, event_id, option_id) {
+  require_auth(user);
+  // check if event_id exists
+  // check if voting for event_id is not closed
+  // check if option_id is a valid option_id.
+
+    distribution_event_registration_table _distribution_event_registration_table(get_self(), get_self().value);
+
+    auto& event_itr = _distribution_event_registration_table.get(event_id, "Invalid eventId");
+    vector<uint32_t> option_ids= event_itr->option_ids;
+    std::vector<int>::iterator option_itr;
+
+    option_itr = find (option_ids.begin(), option_ids.end(), option_id);
+    eosio::check(option_itr != option_itr.end(), "Invalid option");
+   
+    uint64_t event_close_time = event_itr->event_close_time;
+    if(event_close_time<eosio::current_time_point().sec_since_epoch(), "Event voting closed");
+
+    
+
+
 }
 
 ACTION fantasy::selection(name user, uint32_t option_id, uint32_t event_id) {
@@ -49,71 +134,5 @@ ACTION fantasy::selection(name user, uint32_t option_id, uint32_t event_id) {
   }
 }
 
-ACTION fantasy::regevent(
-    uint8_t event_type, 
-    string event_text, 
-    string event_closes_at) {
-  require_auth(get_self());
-  events_table _events_table(get_self(), get_self().value);
-
-    _events_table.emplace(get_self(), [&](auto & row) {
-    row.event_id = _events_table.available_primary_key();
-    row.event_type = event_type;
-    row.event_text = event_text;
-    row.event_closes_at = event_closes_at;
-  });
-}
-
-ACTION fantasy::regoption(
-    uint64_t option_type,
-    uint64_t event_id,
-    uint64_t option_text) {
-
-  require_auth(get_self());
-  events_table _events_table(get_self(), get_self().value);
-  auto& event = _events_table.get(event_id, "Event does not exist");
-  options_table _options_table(get_self(), get_self().value);
-
-    _options_table.emplace(get_self(), [&](auto & row) {
-      row.option_id = _options_table.available_primary_key();
-      row.option_type = option_type;
-      row.event_id = event_id;
-      row.option_text = option_text;
-    });
-}
-
-
-ACTION fantasy::closeevent(uint64_t event_id, uint64_t option_id, string outcome_text){
-  require_auth(get_self());
-  options_table _options_table(get_self(), get_self().value);   
-  auto& option = _options_table.get(option_id, "Option does not exist in options table");
-
-  auto users_index = _options_table.get_index<name("eventkey")>();
-  auto& users_itr = users_index.get(event_id, "Event does not exist in options table");
-
-  events_table _events_table(get_self(), get_self().value);
-  auto& event = _events_table.get(event_id, "Event does not exist in events table");
-
-
-
-
-  _events_table.modify(event, get_self(), [&](auto& row){row.event_status = CLOSED;});
-      
-  outcome_table _outcome_table(get_self(), get_self().value);
-  auto outcome_itr = _outcome_table.find(event_id);
-
-  if(outcome_itr == _outcome_table.end()) {
-        _outcome_table.emplace(get_self(), [&](auto & row) {
-          row.event_id = event_id;
-          row.option_id = option_id;
-          row.outcome_text = outcome_text;});
-  } else {
-        _outcome_table.modify(outcome_itr, get_self(), [&](auto& row){
-          row.option_id = option_id;
-          row.outcome_text = outcome_text;});
-  }
-
-
-}
 
 EOSIO_DISPATCH(fantasy, (adduser)(selection)(regevent)(regoption)(closeevent))
